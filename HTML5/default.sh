296840019719104b4631f28568b0bfb009218800
ARuei <huangyuanruei@gmail.com>
2017-10-26 21:36:41 +0800

    A basic template of html files

---
+++ HTML5/default.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cat << _EOF_
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+    <title>HTML Template example</title>
+	<link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
+_EOF_
